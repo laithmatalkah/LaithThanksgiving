be64dc45643000e3e8b8b07e1dc0c86666d6b707
Laith <laithmatalkah@yahoo.com>
2018-11-21 16:51:20 -0700

    Room Controller

--- src/main/java/com/example/LaithThanksgiving/LaithThanksgiving/Controllers/RoomController.java
+++ src/main/java/com/example/LaithThanksgiving/LaithThanksgiving/Controllers/RoomController.java
@@ -1,4 +1,40 @@
 package com.example.LaithThanksgiving.LaithThanksgiving.Controllers;
 
+
+import com.example.LaithThanksgiving.LaithThanksgiving.data_model.Room;
+import com.example.LaithThanksgiving.LaithThanksgiving.data_service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/room")
 public class RoomController {
+
+    @Autowired
+    RoomService roomService;
+
+    @PostMapping("/create")
+    public ResponseEntity <Room>  createRoom (@RequestBody Room room){
+
+        roomService.createRoom(room);
+        return ResponseEntity.ok(room);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity <Room> getRoomById(@PathVariable ("id") Long id){
+
+        try {
+            Room room = roomService.getRoom(id);
+            return ResponseEntity.ok(room);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+
+
 }
