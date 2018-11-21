754e062984bec021d3d644d4991b3f413ab95d15
Laith <laithmatalkah@yahoo.com>
2018-11-21 16:23:50 -0700

    Room Entity

--- src/main/java/com/example/LaithThanksgiving/LaithThanksgiving/data_service/RoomService.java
+++ src/main/java/com/example/LaithThanksgiving/LaithThanksgiving/data_service/RoomService.java
@@ -1,4 +1,22 @@
 package com.example.LaithThanksgiving.LaithThanksgiving.data_service;
 
+import com.example.LaithThanksgiving.LaithThanksgiving.data_model.Room;
+import com.example.LaithThanksgiving.LaithThanksgiving.repository.RoomRepo;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class RoomService {
+
+
+    private final RoomRepo roomRepo;
+
+    public RoomService(RoomRepo roomRepo) {
+        this.roomRepo = roomRepo;
+    }
+
+    public void createRoom (Room room){ this.roomRepo.save(room);}
+    public Room getRoom (Long id) { return this.roomRepo.findById(id).get();}
+
+
 }
