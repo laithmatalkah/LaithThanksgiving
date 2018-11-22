b1476dd59735160f691dfd88c3dd5282feb9f425
Laith <laithmatalkah@yahoo.com>
2018-11-22 00:55:18 -0700

    Character Test  Added

--- src/test/java/com/example/LaithThanksgiving/LaithThanksgiving/CharControllerTest.java
+++ src/test/java/com/example/LaithThanksgiving/LaithThanksgiving/CharControllerTest.java
@@ -1,14 +1,14 @@
 package com.example.LaithThanksgiving.LaithThanksgiving;
 
 
-import com.example.LaithThanksgiving.LaithThanksgiving.Controllers.CharController;
-import com.example.LaithThanksgiving.LaithThanksgiving.data_service.CharService;
 import com.example.LaithThanksgiving.LaithThanksgiving.data_model.Chars;
+import com.example.LaithThanksgiving.LaithThanksgiving.data_service.CharService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
@@ -24,11 +24,9 @@ CharService charServiceMock= Mockito.mock(CharService.class) ;
     public void createCharTest(){
 
     Chars expected= new Chars ();
-    CharController charController =  new CharController(charServiceMock);
-
-    charController.generateCharacter("Cha1","Warrior",expected);
-
+    charServiceMock.createCharacter(expected);
     verify(charServiceMock, times(1)).createCharacter(expected);
+
 
 }
 
